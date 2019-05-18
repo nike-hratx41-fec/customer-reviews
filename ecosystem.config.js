ef95c24fa4314ddadb3116f5dcf6a6980dbe0bca
lui914 <luirangel914@gmail.com>
2019-05-18 16:16:24 -0500

    fixed path issue in eco file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,8 +8,7 @@ module.exports = {
   deploy: {
     production: {
       user: "ubuntu",
-      host:
-        "ec2-52-209ec2-3-16-213-178.us-east-2.compute.amazonaws.com-166-225.eu-west-1.compute.amazonaws.com",
+      host: "ec2-3-16-213-178.us-east-2.compute.amazonaws.com",
       key: "~/.ssh/FIKE.pem",
       ref: "origin/master",
       repo: "https://github.com/nike-hratx41-fec/customer-reviews",
